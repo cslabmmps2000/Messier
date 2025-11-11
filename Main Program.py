ee3ee61300de794e7e5708d498f97a20023ba694
cslabmmps2000 <cslabmmps2000@gmail.com>
2025-11-11 10:13:46 +0530

    Updated Database Code #1

--- Main Program.py
+++ Main Program.py
@@ -22,12 +22,12 @@ def CreateTable(tablename):
     cursor.execute(create_table)
     db.commit()
 
-def UpdateTable(tablename, messier, name, dist, date, types, constant, mag, desc):
+def UpdateTable(tablename, messier, name, dist, date, types, const, mag, desc):
     cursor = db.cursor()
     
     update_table = f'''INSERT INTO {tablename}
     ( Messier_Number, Name, Distance, Discovery_Date, Object_Type, Constellation, Apparent_Magnitude, Description)
-    VALUES ({messier}, {name}, {dist}, {date}, {types}, {constant}, {mag}, {desc});
+    VALUES ({messier}, {name}, {dist}, {date}, {types}, {const}, {mag}, {desc});
     '''
 
     cursor.execute(update_table)
@@ -53,4 +53,33 @@ while(True):
     time.sleep(0.5)
     
     if (inp == 1):
+        tabname = str(input("Enter the Table name that you wish to create: "))
+        CreateTable(tabname)
+        time.sleep(1)
+        print("Table Created!")
+    
+    elif (inp == 2):
+        tabname = str(input("Enter the Table name that you wish to edit in: "))
+        time.sleep(0.6)
+        mesname = str(input("Please enter the Messier Number: "))
+        time.sleep(0.6)
+        objname = str(input("Enter the Name of the Messier Object: "))
+        time.sleep(0.6)
+        dist = int(input("Please enter the Distance of the Messier Object from Earth in Light-Years: "))
+        time.sleep(0.6)
+        objdate = str(input("Enter theDate that the object was discovered in (Format: DD-MM-YYYY): "))
+        time.sleep(0.6)
+        objtype = str(input("Please enter the Type of the Messier Object: "))
+        time.sleep(0.6)
+        constname = str(input("Please enter the name of the Constellation in which the Messier Object is located in: "))
+        time.sleep(0.6)
+        apmag = float(input("Please enter the Apparent Magnitude of the Messier Object: "))
+        time.sleep(0.6)
+        desc = str(input("Please enter a short Description of the Messier Object: "))
+        time.sleep(0.6)
+        UpdateTable(tabname, mesname, objname, dist, objdate, objtype, constname, apmag, desc)
+        time.sleep(1)
+        print("Table Updated!")
+
+    elif (inp == 3):
         pass
