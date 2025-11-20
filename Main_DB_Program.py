386e9d6c2932192b7a50b1a49a87213fcf4434a8
CS Lab Comp (Taha) <cslabmmps>
2025-11-20 10:31:49 +0530

    Updated to the Latest Version of the Project

--- Main_DB_Program.py
+++ Main_DB_Program.py
@@ -1,9 +1,9 @@
 import sqlite3 as sql
 import time
-import getpass
 import os
 
-os.chdir(f"C:/Users/{getpass.getuser()}/Messier")
+dir = str(input("Please enter the Directory location of the Project Folder: "))
+os.chdir(dir)
 db = sql.connect("The_Messier_Dualis_Database.db")
 
 def CreateTable():
@@ -28,21 +28,54 @@ def CreateTable():
     cursor.execute(create_table)
     db.commit()
 
+def ShowDB():
+    tabname = str(input("Please enter the name of the table that you wish to view: "))
+    time.sleep(0.5)
+    cursor = db.cursor()
+    showDB = f"SELECT * FROM {tabname}"
+
+    cursor.execute(showDB)
+
+    rows = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+    print(column_names)
+    time.sleep(0.4)
+    for row in rows:
+        print(row)
+    db.commit()
+
+def ShowDBParticular():
+    tabname = str(input("Please enter the name of the table that you wish to view: "))
+    mesnum = str(input("Please enter the Messier Number that you wish to see the Records of: "))
+    time.sleep(0.5)
+    cursor = db.cursor()
+    showDBParticular = f"SELECT * FROM {tabname} WHERE Messier_Number = '{mesnum}'"
+
+    cursor.execute(showDBParticular)
+
+    rows = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+    print(column_names)
+    time.sleep(0.4)
+    for row in rows:
+        print(row)
+    db.commit()
+
 def UpdateTable(tablename, messier, name, dist, date, discoverer, types, const, mag, desc, fun, img):
     cursor = db.cursor()
     
     update_table = f'''INSERT INTO {tablename}
     (Messier_Number, Name, Distance, Discovery_Date, Discoverer, Object_Type, Constellation, Apparent_Magnitude, Description, Fun_Fact, Image_File)
-    VALUES ({messier}, {name}, {dist}, {date}, {discoverer}, {types}, {const}, {mag}, {desc}, {fun}, {img});
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
     '''
 
-    cursor.execute(update_table)
+    cursor.execute(update_table, (messier, name, dist, date, discoverer, types, const, mag, desc, fun, img))
     db.commit()
 
 def DelRow(tablename, messier):
     cursor = db.cursor()
     
-    del_row = f"DELETE FROM {tablename} WHERE Messier_Number = {messier};"
+    del_row = f"DELETE FROM {tablename} WHERE Messier_Number = '{messier}';"
 
     cursor.execute(del_row)
     db.commit()
@@ -53,17 +86,21 @@ time.sleep(1)
 
 while(True):
     print("1. Create a Table")
-    time.sleep(1)
-    print("2. Update a Record in the Table")
-    time.sleep(1)
-    print("3. Delete a Record in the Table")
-    time.sleep(1)
-    print("4. Exit the Program")
-    time.sleep(1)
-    inp = int(input("Please enter the number of the operation that you want to perform: "))
-    time.sleep(1)
-    print("Executing!...")
     time.sleep(0.5)
+    print("2. Update a Record in the Table")
+    time.sleep(0.5)
+    print("3. Delete a Record in the Table")
+    time.sleep(0.5)
+    print("4. Show the Entire Table")
+    time.sleep(0.5)
+    print("5. Show a Particular record from the Table")
+    time.sleep(0.5)
+    print("6. Exit the Program")
+    time.sleep(0.5)
+    inp = int(input("Please enter the number of the operation that you want to perform: "))
+    time.sleep(0.5)
+    print("Executing!...")
+    time.sleep(0.3)
     
     if (inp == 1):
         CreateTable()
@@ -71,15 +108,15 @@ while(True):
         print("Table Created! :D")
     
     elif (inp == 2):
-        username = getpass.getuser()
-        Tablename = str(input("Enter the Table name that you wish to create: "))
+        Tablename = str(input("Enter the Table name that you wish to Edit into: "))
+        time.sleep(1)
         mesname = str(input("Please enter the Messier Number: "))
         time.sleep(0.6)
         objname = str(input("Enter the Name of the Messier Object: "))
         time.sleep(0.6)
         dist = int(input("Please enter the Distance of the Messier Object from Earth in Light-Years: "))
         time.sleep(0.6)
-        objdate = str(input("Enter the Date that the object was discovered in (Format: YYYY): "))
+        objdate = str(input("Enter the Year that the object was discovered in (Format: YYYY): "))
         time.sleep(0.6)
         objdisc = str(input("Enter the Full Name of the Discoverer of the Messier Object: "))
         time.sleep(0.6)
@@ -95,7 +132,7 @@ while(True):
         time.sleep(0.6)
         img = str(input("Please enter the Filename of the Image of the Messier Object: "))
         time.sleep(0.6)
-        img2 = f"C:/Users/{username}/Messier/Image Files/" + {img}
+        img2 = dir + '/' + "Image Files/" + img
         UpdateTable(Tablename, mesname, objname, dist, objdate, objdisc, objtype, constname, apmag, desc, funfac, img2)
         time.sleep(1)
         print("Table Updated! :D")
@@ -108,10 +145,27 @@ while(True):
         DelRow(Tablename, mesname)
         time.sleep(1)
         print("Editing Complete! :D")
-        
+
     elif (inp == 4):
+        time.sleep(1)
+        ShowDB()
+        time.sleep(1)
+        print("Done!")
+
+    elif (inp == 5):
+        time.sleep(0.4)
+        ShowDBParticular()
+        time.sleep(1)
+        print("Done!")
+        
+    elif (inp == 6):
         print("Exiting!...")
+        time.sleep(1)
+        print("Thanks a lot for using my Program!")
+        time.sleep(1)
+        print("Made with Love by Taha :D")
         time.sleep(1)
         break
     
 db.close()
+exit()
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Messier
-(This is the Repository of The Messier Dualis)
+# The-Messier-Dualis
+(This is the Repository of 'The Messier Dualis' Python Project as my Final Project of my Computer Science Class (Highschool))
 
 'Messier Dualis' is a Interactive GUI Based Project about the Messier List of Astronomical Objects (About 110 Objects) in the night sky
+
+{More Info to be Added... :D}
