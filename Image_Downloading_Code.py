98ec11a50af60b28fe147111395298bded065bd6
IP Lab MMPS <cslabmmps2000@gmail.com>
2025-11-21 13:12:47 +0530

    Useless changes

--- Image_Downloading_Code.py
+++ Image_Downloading_Code.py
@@ -2,10 +2,24 @@ import requests as req
 import shutil as shush
 import time
 
-URLs = f"https://osricdienda.com/messier-api/images/M{num}.jpg"
+URLs = "https://osricdienda.com/messier-api/images/M{num}.jpg"
 for i in range(1, 111):
-    print(f"Downloading Image for Messier {i}")
-    Response = req.get(URLs(num=i), stream=True)
+    print(f"Downloading Image for Messier {i}... /")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... |")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... \\")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... --")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... /")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... |")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... \\")
+    time.sleep(0.1)
+    print(f"Downloading Image for Messier {i}... --")
+    Response = req.get(URLs.format(num = i), stream=True)
     f = open(f'Image Files/Messier_{i}.jpg', 'wb')
     shush.copyfileobj(Response.raw, f)
     f.close()
